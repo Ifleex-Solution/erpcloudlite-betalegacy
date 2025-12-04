02f420361ceb9abbb5629d8334adc362b514ec29
Ifleex-Solution <ifleexinternal@gmail.com>
2025-12-04 10:02:23 +0530

    missing files adding

--- application/config/database.php
+++ application/config/database.php
@@ -4,13 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 $active_record = TRUE;//ci version 2.x
+// $db['default'] = array(
+//     'dsn'   => '',
+//     'hostname' => 'localhost',
+//     'username' => 'root',
+//     'password' => '',
+//     'database' => 'betalitelegacy',
+//     'dbdriver' => 'mysqli',
+//     'max_allowed_packet'=>'200M',
+//     'dbprefix' => '',
+//     'pconnect' => FALSE,
+//     'db_debug' => (ENVIRONMENT !== 'production'),
+//     'cache_on' => FALSE,
+//     'cachedir' => '',
+//     'char_set' => 'utf8',
+//     'dbcollat' => 'utf8_general_ci',
+//     'swap_pre' => '',
+//     'encrypt'  => FALSE,
+//     'compress' => FALSE,
+//     'autoinit' => TRUE,//ci version 2.x
+//     'stricton' => FALSE,
+//     'failover' => array(),
+//     'save_queries' => TRUE
+// );
 
 $db['default'] = array(
     'dsn'   => '',
     'hostname' => 'localhost',
     'username' => 'fextenso_admin',
     'password' => '4IB49p74odV8',
-    'database' => 'fextenso_erplitemarketing',
+    'database' => 'fextenso_erplitelegacy',
     'dbdriver' => 'mysqli',
     'max_allowed_packet'=>'200M',
     'dbprefix' => '',
